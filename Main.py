3fe5138091af8fafdce9f028cc1be22065d548d9
Cassius P <287ilias@gmail.com>
2020-12-16 17:07:45 +0100

    Grid lvl 5

--- Main.py
+++ Main.py
@@ -1,15 +1,39 @@
 from tkinter import *
+from math import *
+
+def clicked(event):
+    print("pressed")
 
 def main():
-    fenetre = Tk()
+    value = 9
 
-    label = Label(fenetre, text="Hello World")
+    fenetre = Tk(className="Sudoku", screenName="Sudoku", baseName="Sudoku")
+    fenetre.geometry("1000x700")
+
+    canvas = Canvas(fenetre, relief=FLAT, background="black", width=value*100+sqrt(value), height=value*100+sqrt(value))
+    canvas.pack()
+    label = Label(fenetre, text="Hello world")
     label.pack()
+
+    for i in range(value):
+        for j in range(value):
+            blockI = 99
+            blockJ = 99
+            if (j % sqrt(value) == 0):
+                blockJ = 95
+
+            if (i % sqrt(value) == 0):
+                blockI = 95
+
+            coordinatesX = 100 * (j + 1)
+            coordinatesY = 100 * (i + 1)
+            canvas.create_rectangle(coordinatesX - blockJ, coordinatesY - blockI, coordinatesX, coordinatesY,
+                                    fill="white", outline="black")
+            
 
     fenetre.mainloop()
 
 main()
-
 
 
 
